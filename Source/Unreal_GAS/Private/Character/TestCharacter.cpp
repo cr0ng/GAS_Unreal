f61b2b351cbafa7b5bc8dbee78df56456535a77a
yujin <yj98k@naver.com>
2026-01-05 13:53:16 +0900

    ASC Component 추가

---
+++ Source/Unreal_GAS/Private/Character/TestCharacter.cpp
@@ -0,0 +1,75 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/TestCharacter.h"
+#include "AbilitySystemComponent.h"
+#include "GameAbilitySystem/StatusAttributeSet.h"
+
+// Sets default values
+ATestCharacter::ATestCharacter()
+{
+	PrimaryActorTick.bCanEverTick = true;
+
+	// 컴포넌트 생성
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+
+	// 어트리뷰트 셋 생성
+	StatusAttributeSet = CreateDefaultSubobject<UStatusAttributeSet>(TEXT("Status"));
+}
+
+void ATestCharacter::TestHealthChange(float Amount)
+{
+	if (StatusAttributeSet)
+	{
+		float CurrentValue = StatusAttributeSet->GetHealth();
+		StatusAttributeSet->SetHealth(CurrentValue + Amount);
+	}
+}
+
+// Called when the game starts or when spawned
+void ATestCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	if (AbilitySystemComponent)
+	{
+		AbilitySystemComponent->InitAbilityActorInfo(this, this); // 어빌리티 시스템 컴포넌트 초기화
+		
+		// 초기화 이후에만 가능
+		FOnGameplayAttributeValueChange& onHealthChange =
+			AbilitySystemComponent->GetGameplayAttributeValueChangeDelegate(UStatusAttributeSet::GetHealthAttribute());
+	
+		onHealthChange.AddUObject(this, &ATestCharacter::OnHealthChange); // Health가 변경되었을 때 실행될 함수 바인딩
+	}
+
+	//if (StatusAttributeSet)
+	//{
+	//	//StatusAttributeSet->Health = 10.0f;	// 절대 안 됨
+	//	StatusAttributeSet->SetHealth(50.0f);	// 무조건 Setter로 변경해야 한다.
+	//}
+}
+
+// Called every frame
+void ATestCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	FString healthString = FString::Printf(TEXT("%.1f / %.1f"),
+		StatusAttributeSet->GetHealth(), StatusAttributeSet->GetMaxHealth());
+	//UE_LOG(LogTemp, Log, TEXT("%s"), *healthString);
+	DrawDebugString(GetWorld(), GetActorLocation(), healthString,nullptr,FColor::White,0,true);
+}
+
+// Called to bind functionality to input
+void ATestCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+}
+
+void ATestCharacter::OnHealthChange(const FOnAttributeChangeData& InData)
+{
+	UE_LOG(LogTemp, Log, TEXT("Health Change : %.1f-> %.1f"), InData.OldValue, InData.NewValue);
+
+}
+
---
+++ Source/Unreal_GAS/Public/Character/TestCharacter.h
@@ -0,0 +1,47 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "GameplayEffectTypes.h"
+#include "TestCharacter.generated.h"
+
+class UStatusAttributeSet;
+
+UCLASS()
+class UNREAL_GAS_API ATestCharacter : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ATestCharacter();
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override {
+		return AbilitySystemComponent;
+	};
+
+	UFUNCTION(BlueprintCallable)
+	void TestHealthChange(float Amount);
+
+protected:	
+	virtual void BeginPlay() override;
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+private:
+	void OnHealthChange(const FOnAttributeChangeData& InData);
+
+public:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Test")
+	float TestValue = 10.0f;
+
+protected:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = "Ability")
+	TObjectPtr<UAbilitySystemComponent>AbilitySystemComponent = nullptr;
+
+private:
+	UPROPERTY()
+	TObjectPtr<UStatusAttributeSet> StatusAttributeSet = nullptr;
+};
