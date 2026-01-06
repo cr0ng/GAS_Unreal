f19e059f19dc155ad77ea0c470d221922cb221b1
yujin <yj98k@naver.com>
2026-01-06 20:55:14 +0900

    오류 발생해서 선생님껄로..

---
+++ Source/KI7_UnrealGAS/Private/UI/BarWidget.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "UI/BarWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/ProgressBar.h"
+
+void UBarWidget::SetProgressColor(FLinearColor InColor)
+{
+	BackgroundProgressBar->SetFillColorAndOpacity(InColor);
+}
+
+void UBarWidget::UpdateCurrent_Implementation(float InValue)
+{
+	UE_LOG(LogTemp, Log, TEXT("Current : %.1f"), InValue);
+	Current->SetText(FText::AsNumber(FMath::RoundToInt(InValue)));
+	CurrentValue = InValue;
+	BackgroundProgressBar->SetPercent(CurrentValue /MaxValue);
+}
+
+void UBarWidget::UpdateMax_Implementation(float InValue)
+{
+	UE_LOG(LogTemp, Log, TEXT("Max : %.1f"), InValue);
+	Max->SetText(FText::AsNumber(FMath::RoundToInt(InValue)));
+	MaxValue = InValue;
+	BackgroundProgressBar->SetPercent(CurrentValue / MaxValue);
+}
